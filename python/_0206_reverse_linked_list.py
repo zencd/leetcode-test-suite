450a9922f8d5a5e4b6bc9a741971d501312baa8b
zzz <z@z.com>
2026-08-31 16:41:31 +0300

    fix 206

--- python/_0206_reverse_linked_list.py
+++ python/_0206_reverse_linked_list.py
@@ -3,6 +3,7 @@
 # Easy
 
 from typing import Optional
+import unittest
 
 
 class ListNode:
@@ -16,101 +17,76 @@ class Solution:
         raise Exception("Not solved yet")
 
 
-def make_list(values):
+def build_list(values):
     head = None
-    for v in reversed(values):
-        head = ListNode(v, head)
+    tail = None
+    for v in values:
+        node = ListNode(v)
+        if head is None:
+            head = node
+            tail = node
+        else:
+            tail.next = node
+            tail = node
     return head
 
 
 def to_list(head):
-    result = []
+    values = []
+    seen = set()
     while head is not None:
-        result.append(head.val)
+        if id(head) in seen:
+            raise AssertionError("cycle detected in linked list")
+        seen.add(id(head))
+        values.append(head.val)
         head = head.next
-    return result
-
-
-import unittest
+    return values
 
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
-        self.sol = Solution()
+        self.solution = Solution()
 
     def test_empty_list(self):
-        self.assertIsNone(self.sol.reverseList(None))
+        self.assertIsNone(self.solution.reverseList(None))
 
     def test_single_node(self):
-        head = make_list([5])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [5])
+        head = build_list([42])
+        self.assertEqual(to_list(self.solution.reverseList(head)), [42])
+        self.assertIsNone(head.next)
 
     def test_two_nodes(self):
-        head = make_list([1, 2])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [2, 1])
+        head = build_list([1, 2])
+        result = self.solution.reverseList(head)
+        self.assertEqual(to_list(result), [2, 1])
+        self.assertIsNone(result.next.next)
 
-    def test_example_one(self):
-        head = make_list([1, 2, 3, 4, 5])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [5, 4, 3, 2, 1])
+    def test_three_nodes(self):
+        head = build_list([1, 2, 3])
+        self.assertEqual(to_list(self.solution.reverseList(head)), [3, 2, 1])
 
-    def test_all_same_values(self):
-        head = make_list([7, 7, 7])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [7, 7, 7])
+    def test_example1(self):
+        head = build_list([1, 2, 3, 4, 5])
+        self.assertEqual(to_list(self.solution.reverseList(head)), [5, 4, 3, 2, 1])
+
+    def test_reverse_does_not_create_cycle(self):
+        head = build_list([1, 2, 3])
+        self.solution.reverseList(head)
+        to_list(head)
 
     def test_negative_values(self):
-        head = make_list([-5000, 0, 5000])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [5000, 0, -5000])
+        head = build_list([-5000, 0, 5000])
+        self.assertEqual(to_list(self.solution.reverseList(head)), [5000, 0, -5000])
 
-    def test_mixed_signs(self):
-        head = make_list([1, -1, 2, -2, 3])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [3, -2, 2, -1, 1])
+    def test_duplicate_values(self):
+        head = build_list([7, 7, 7, 7])
+        self.assertEqual(to_list(self.solution.reverseList(head)), [7, 7, 7, 7])
 
-    def test_zero_values(self):
-        head = make_list([0, 0])
-        self.assertEqual(to_list(self.sol.reverseList(head)), [0, 0])
-
-    def test_original_list_disconnected(self):
-        head = make_list([1, 2, 3])
-        new_head = self.sol.reverseList(head)
-        self.assertEqual(head.next, None)
-        self.assertEqual(to_list(new_head), [3, 2, 1])
-
-    def test_many_nodes(self):
-        values = list(range(1, 5001))
-        head = make_list(values)
-        self.assertEqual(to_list(self.sol.reverseList(head)), list(reversed(values)))
-
-    def test_many_nodes_negative_range(self):
-        values = list(range(-5000, 0))
-        head = make_list(values)
-        self.assertEqual(to_list(self.sol.reverseList(head)), list(reversed(values)))
-
-    def test_recursive_variant_empty(self):
-        self.assertIsNone(Solution()._reverse_recursive(None))
-
-    def test_recursive_variant_single(self):
-        head = make_list([9])
-        self.assertEqual(to_list(Solution()._reverse_recursive(head)), [9])
-
-    def test_recursive_variant_two(self):
-        head = make_list([1, 2])
-        self.assertEqual(to_list(Solution()._reverse_recursive(head)), [2, 1])
-
-    def test_recursive_variant_example_one(self):
-        head = make_list([1, 2, 3, 4, 5])
-        self.assertEqual(to_list(Solution()._reverse_recursive(head)), [5, 4, 3, 2, 1])
-
-    def test_recursive_variant_disconnected(self):
-        head = make_list([1, 2, 3])
-        new_head = Solution()._reverse_recursive(head)
-        self.assertEqual(head.next, None)
-        self.assertEqual(to_list(new_head), [3, 2, 1])
-
-    def test_recursive_many_nodes(self):
-        values = list(range(100, 0, -1))
-        head = make_list(values)
+    def test_long_list(self):
+        values = list(range(5000))
+        head = build_list(values)
         self.assertEqual(
-            to_list(Solution()._reverse_recursive(head)), list(reversed(values))
+            to_list(self.solution.reverseList(head)), list(reversed(values))
         )
 
 
